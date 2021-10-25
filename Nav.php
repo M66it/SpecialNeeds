24f720ff5d2691848d443a896191c6032f6d3a3f
yousef33i <78238700+yousef33i@users.noreply.github.com>
2021-10-25 13:52:28 +0300

    nav

---
+++ Nav.php
@@ -0,0 +1,25 @@
+							<nav id="nav">
+								<ul>
+									<li class="current"><a href="index.php">الصفحة الرئيسية</a></li>
+									<li>
+										الحساب
+										<ul>
+											<li><a href="#">معلومات الحساب</a></li>
+											<li><a href="#">الطلبات</a></li>
+											<li>
+												<a href="#">التبرعات</a>
+												<ul>
+													<li><a href="#">طلب تبرع بالمستلزمات</a></li>
+													<li><a href="#">طلب احتياج مستلزمات</a></li>
+													<li><a href="#">قائمة الطلبات</a></li>
+												</ul>
+											</li>
+											<li><a href="SignOut.php">تسجيل الخروج</a></li>
+										</ul>
+									</li>
+									<li><a href="">المستلزمات الجديدة</a></li>
+									<li><a href="">المستلزمات المستعملة</a></li>
+									<li><a href="SignIn.php">تسجيل الدخول</a></li>
+									<li><a href="">التواصل</a></li>
+								</ul>
+							</nav>
