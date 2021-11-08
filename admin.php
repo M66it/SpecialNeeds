dce8c07c4ae7f64ddc1244aa6723a04336f790fb
TalalMaghrabi <tlalhesham@gmail.com>
2021-11-08 12:09:11 +0300

    add footer to admin

--- admin.php
+++ admin.php
@@ -410,6 +410,58 @@
             </div>
         </main>
 
+
+
+        	<!-- الجزء السفلي -->
+			<footer class="footer">
+				<div class="container">
+					<div class="row">
+						<div class="footer-col">
+							<h4>الموقع</h4>
+							<ul>
+								<li><a href="#">about us</a></li>
+								<li><a href="#">our services</a></li>
+								<li><a href="#">privacy policy</a></li>
+								
+							</ul>
+						</div>
+						<div class="footer-col">
+							<h4>المساعدة</h4>
+							<ul>
+								<li><a href="#">FAQ</a></li>
+								<li><a href="#">shipping</a></li>
+								<li><a href="#">returns</a></li>
+								<li><a href="#">order status</a></li>
+								<li><a href="#">payment options</a></li>
+							</ul>
+						</div>
+						
+					
+						<div class="footer-col">
+							<h4>طرق التواصل</h4>
+							<div class="social-links">
+								<a href="#"><i class="fab fa-facebook-f"></i></a>
+								<a href="#"><i class="fab fa-twitter"></i></a>
+								<a href="#"><i class="fab fa-instagram"></i></a>
+
+								
+							</div>
+							
+							
+						</div>
+					</div>
+					<div  class="footer66">
+						<p> designed by <span>ali-Talal-Mohamed-yousef 2021</span></p>
+					</div>
+				</div>
+							
+							</div>
+						</div>
+					</footer>
+				</div>
+
+			</div>
+
     </div>
 	
 
